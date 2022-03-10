946172be50652f8002de84462f85fcded3e98e92
fikridzakwan <dikri280d@gmail.com>
2022-03-10 17:05:30 +0700

    Update# Update detail movie with animation

--- app/src/main/java/co/id/fikridzakwan/somethingbig/presentation/detail/DetailMovieActivity.kt
+++ app/src/main/java/co/id/fikridzakwan/somethingbig/presentation/detail/DetailMovieActivity.kt
@@ -1,16 +1,16 @@
 package co.id.fikridzakwan.somethingbig.presentation.detail
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
+import android.content.res.Configuration
+import android.graphics.Color
 import androidx.activity.viewModels
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.lifecycle.lifecycleScope
-import co.id.fikridzakwan.somethingbig.BuildConfig
+import androidx.transition.TransitionManager
 import co.id.fikridzakwan.somethingbig.R
-import co.id.fikridzakwan.somethingbig.customview.LoadingCustomView
 import co.id.fikridzakwan.somethingbig.customview.gone
 import co.id.fikridzakwan.somethingbig.customview.loadImage
 import co.id.fikridzakwan.somethingbig.customview.visible
@@ -19,17 +19,14 @@ import co.id.fikridzakwan.somethingbig.databinding.ActivityDetailMovieBinding
 import co.id.fikridzakwan.somethingbig.domain.model.Detail
 import co.id.fikridzakwan.somethingbig.utils.AppConstants.EXTRA_ID
 import co.id.fikridzakwan.somethingbig.utils.BaseActivity
-import co.id.fikridzakwan.somethingbig.utils.makeStatusBarTransparent
 import co.id.fikridzakwan.somethingbig.utils.resetStatusBarColor
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 @AndroidEntryPoint
 class DetailMovieActivity : BaseActivity<ActivityDetailMovieBinding>() {
 
     private val viewModel: DetailMovieViewModel by viewModels()
+    private var isCover = false
 
     companion object {
         fun start(context: Context, id: Int) {
@@ -47,9 +44,7 @@ class DetailMovieActivity : BaseActivity<ActivityDetailMovieBinding>() {
 
     override fun initAction() {
         binding.apply {
-            btnBack.setOnClickListener {
-                onBackPressed()
-            }
+            detailAnimation()
         }
     }
 
@@ -62,7 +57,9 @@ class DetailMovieActivity : BaseActivity<ActivityDetailMovieBinding>() {
         lifecycleScope.launchWhenStarted {
             viewModel.getDetail.collect {
                 when (it) {
-                    is Resource.Loading -> { binding.progressBar.visible() }
+                    is Resource.Loading -> {
+                        binding.progressBar.visible()
+                    }
                     is Resource.Success -> {
                         binding.progressBar.gone()
                         it.data?.let { v -> populateDetail(v) }
@@ -86,6 +83,81 @@ class DetailMovieActivity : BaseActivity<ActivityDetailMovieBinding>() {
             tvDate.text = data.releaseDate
             tvRuntime.text = data.runtime
             tvOverview.text = data.overview
+        }
+    }
+
+    private fun detailAnimation() {
+        val coverConstraintSet = ConstraintSet()
+        coverConstraintSet.clone(this, R.layout.full_image_movie_detail)
+
+        val initConstraintSet = ConstraintSet()
+        initConstraintSet.clone(this, R.layout.activity_detail_movie)
+
+        binding.imgPoster.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.root)
+            coverConstraintSet.applyTo(binding.root)
+
+            val anim = ValueAnimator()
+            anim.setIntValues(Color.BLACK, Color.WHITE)
+            anim.setEvaluator(ArgbEvaluator())
+            anim.addUpdateListener {
+                binding.apply {
+                    imageCalendar.setColorFilter(it.animatedValue as Int)
+                    imageTime.setColorFilter(it.animatedValue as Int)
+                    tvTitle.setTextColor(it.animatedValue as Int)
+                    tvRuntime.setTextColor(it.animatedValue as Int)
+                    tvDate.setTextColor(it.animatedValue as Int)
+                }
+            }
+            anim.duration = 300
+            anim.start()
+            isCover = true
+        }
+
+        binding.btnBack.setOnClickListener {
+            if (isCover) {
+                TransitionManager.beginDelayedTransition(binding.root)
+                initConstraintSet.applyTo(binding.root)
+
+                val anim = ValueAnimator()
+                anim.setIntValues(Color.WHITE, Color.BLACK)
+                anim.setEvaluator(ArgbEvaluator())
+                anim.addUpdateListener {
+                    val configuration = Configuration()
+                    val currentNightMode = this.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)
+
+                    binding.apply {
+                        when (currentNightMode) {
+                            Configuration.UI_MODE_NIGHT_YES -> {
+                                imageCalendar.setColorFilter(Color.WHITE)
+                                imageTime.setColorFilter(Color.WHITE)
+                                tvTitle.setTextColor(Color.WHITE)
+                                tvRuntime.setTextColor(Color.WHITE)
+                                tvDate.setTextColor(Color.WHITE)
+                            }
+                            Configuration.UI_MODE_NIGHT_NO -> {
+                                imageCalendar.setColorFilter(Color.BLACK)
+                                imageTime.setColorFilter(Color.BLACK)
+                                tvTitle.setTextColor(Color.BLACK)
+                                tvRuntime.setTextColor(Color.BLACK)
+                                tvDate.setTextColor(Color.BLACK)
+                            }
+                            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                                imageCalendar.setColorFilter(Color.BLACK)
+                                imageTime.setColorFilter(Color.BLACK)
+                                tvTitle.setTextColor(Color.BLACK)
+                                tvRuntime.setTextColor(Color.BLACK)
+                                tvDate.setTextColor(Color.BLACK)
+                            }
+                        }
+                    }
+                }
+                anim.duration = 300
+                anim.start()
+                isCover = false
+            } else {
+                onBackPressed()
+            }
         }
     }
 }
